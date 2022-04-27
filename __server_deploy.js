a3af1a06644b161546f0e563827056413dc6b129
Rajesh-107 <applerb326@gmail.com>
2022-04-27 20:40:17 +0600

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,15 @@
+/**
+ * one time in your laptop
+ * ----------------
+ * 
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ---------------
+ * For each project one time
+ * -----------------
+ * 1.heroku create
+ * 2. 
+ * 
+ */
